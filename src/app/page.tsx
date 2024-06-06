e60f563820ff5af8587d6955ff9608a440e3975f
Grind <bellenlowell@gmail.com>
2024-06-06 20:53:28 +0800

    updated title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+"use client"
 
-import Guide from "@/components/features/Guide";
-import Navbar from "@/components/features/Navbar";
-import PlayfulButton from "@/components/ui/PlayfulButton";
-import { useRouter } from "next/navigation";
+import Guide from "@/components/features/Guide"
+import Navbar from "@/components/features/Navbar"
+import PlayfulButton from "@/components/ui/PlayfulButton"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
     <main>
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="dark:bg-[#0f1214] bg-white flex min-h-screen flex-col items-center p-24">
         <div className="text-lg">🦍🐸🦟</div>
         <p className="text-3xl font-light mb-5 font-mono dark:text-neutral-200">
-          HARAMBE V PEPE V HECTOR
+          HARAMBE VS CAYLE
         </p>
         <div className="flex gap-2">
           <PlayfulButton onClick={() => router.push("/play")}>
@@ -25,5 +25,5 @@ export default function Home() {
         <Guide />
       </div>
     </main>
-  );
+  )
 }
