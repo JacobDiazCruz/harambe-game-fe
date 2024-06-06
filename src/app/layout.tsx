1bc44fc34f85c5ef3d02c95c7ec80b22039d084f
Grind <bellenlowell@gmail.com>
2024-06-06 21:46:21 +0800

    fixed theme bug after refresh, added ongoing game feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import GlobalPlatformComponents from "@/components/features/GlobalPlatformComponents";
-import AppContextProvider from "@/store";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import GlobalPlatformComponents from "@/components/features/GlobalPlatformComponents"
+import AppContextProvider from "@/store"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Sol game",
-  description: "",
-};
+  description: ""
+}
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} dark:bg-[#0f1214]`}>
         <AppContextProvider>
           <GlobalPlatformComponents />
           {children}
         </AppContextProvider>
       </body>
     </html>
-  );
+  )
 }
